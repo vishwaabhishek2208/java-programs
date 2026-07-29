b4f62f56c9943513729465a19be833d1bfe333bd
vishwaabhishek2208 <abhishekvishwa2208@gmail.com>
2026-07-29 15:43:45 +0530

    first Java program (intro.java)
    Uploaded my first Java program that prints my name and college details.

---
+++ intro.java
@@ -0,0 +1,9 @@
+public class intro {
+    public static void main(String[] args) {
+
+        System.out.println("Abhishek Vishwakarma");
+        System.out.println("Pursuing Btech CSE from LNCTS BHOPAL");
+
+    }
+
+}
