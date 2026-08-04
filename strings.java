53fe17d1200212762b47c5133cb011be5e21a892
Abhishek Vishwakarma <abhishekvishwa2208@gmail.com>
2026-08-05 02:34:13 +0530

    Add strings.java to concatenate first and last names

---
+++ strings.java
@@ -0,0 +1,14 @@
+public class strings {
+    public static void main(String[] args) {
+
+        // Strings
+        String firstName = "Abhishek";
+        String lastName = "Vishwakarma";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+
+    }
+
+}
