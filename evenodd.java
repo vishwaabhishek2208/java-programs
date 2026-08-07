5dfca61f9aa96dcb216f4c749da5bcdeb7f1e908
Abhishek Vishwakarma <abhishekvishwa2208@gmail.com>
2026-08-07 23:52:41 +0530

    Add evenodd program to check even or odd numbers

---
+++ evenodd.java
@@ -0,0 +1,10 @@
+    public static void main(String[] args) {
+        int i = 7;
+        if (i % 2 == 0) {
+            System.out.println("i is even");
+        } else {
+            System.out.println("i is odd");
+        }
+    }
+
+}
