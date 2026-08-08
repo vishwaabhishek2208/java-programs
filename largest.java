7ab22010c1b36620c3c962095035c8435bbdaf87
Abhishek Vishwakarma <abhishekvishwa2208@gmail.com>
2026-08-08 14:39:50 +0530

    Add program to find the largest of three numbers

---
+++ largest.java
@@ -0,0 +1,15 @@
+public class largest {
+    public static void main(String[] args) {
+        int i = 15;
+        int j = 20;
+        int k = 25;
+        if (i > j && i > k) {
+            System.out.println("i is greatest");
+        } else if (j > i && j > k) {
+            System.out.println("j is greatest");
+        } else {
+            System.out.println("k is greatest");
+        }
+    }
+
+}
