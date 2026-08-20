39251bf90fa3875fb1eff90f917894b6c34ab690
Abhishek Vishwakarma <abhishekvishwa2208@gmail.com>
2026-08-20 22:22:19 +0530

    Add Autoboxing and Unboxing example in Java

---
+++ WrapperClasses/Autoboxing.java
@@ -0,0 +1,36 @@
+public class unautobox {
+    public static void main(String[] args) {
+
+        // Autoboxing
+        int x = 10;
+        Integer y = x; // autoboxing
+
+        System.out.println(x);
+        System.out.println(y); // unboxing
+
+        // Unboxing
+        Integer a = 20; // Autoboxing
+        int b = a; // unboxing
+
+        System.out.println(a); // unboxing
+        System.out.println(b);
+
+        int m = 50;
+        printInteger(m);
+
+        Integer c = 10;
+        Integer d = 20;
+
+        int sum = c.intValue() + d.intValue();
+        System.out.println(sum);
+
+        Integer p = null;
+        int q = p;
+
+        System.out.println(q);
+    }
+
+    static void printInteger(Integer x) {
+        System.out.println(x);
+    }
+}
