09a1188786f9d0c0c64ce1c484d8a3ea99de0550
Abhishek Vishwakarma <abhishekvishwa2208@gmail.com>
2026-09-13 09:45:21 +0530

    Add Demo2.java to read and print a character

---
+++ Basics/Scannerclass/Demo2.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Demo2 {
+    public static void main(String[] args) throws IOException {
+        int x = System.in.read();
+
+        System.out.println((char) x);
+    }
+
+}
+
+// A --> Binary form of 65
