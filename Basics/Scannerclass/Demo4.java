c5516357250b5ce8d707a8c779fb48e81a725683
Abhishek Vishwakarma <abhishekvishwa2208@gmail.com>
2026-09-13 09:46:47 +0530

    Add Demo4.java to read user input and display it

---
+++ Basics/Scannerclass/Demo4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Demo4 {
+    public static void main(String[] args ) {
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+        String name = sc.nextLine();
+
+        System.out.println("age = " + age);
+        System.out.println("Name = " + name);
+    }
+    
+}
