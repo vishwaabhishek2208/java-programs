0cf4099ed0c6df5a64e3310919b1586594771f8b
Abhishek Vishwakarma <abhishekvishwa2208@gmail.com>
2026-09-25 13:52:22 +0530

    Add Demo3 class with Direction enum implementation

---
+++ Enum/Demo3.java
@@ -0,0 +1,25 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        Direction d = Direction.NORTH;
+
+        System.out.println(d.getDegree());
+    }
+
+}
+
+enum Direction {
+    NORTH(0),
+    SOUTH(180),
+    EAST(90),
+    WEST(270);
+
+    private int degree;
+
+    Direction(int degree) {
+        this.degree = degree;
+    }
+
+    public int getDegree() {
+        return this.degree;
+    }
+}
